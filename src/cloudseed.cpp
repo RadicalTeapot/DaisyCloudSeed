efd06a1f1f4def0a4dedafa39a2c42e54eb1e546
Mathias Capdet <35329469+RadicalTeapot@users.noreply.github.com>
2023-05-30 23:05:32 +0200

    Add performance monitoring and change enum to class

--- src/cloudseed.cpp
+++ src/cloudseed.cpp
@@ -9,21 +9,23 @@
 #include "../../CloudSeed/AudioLib/ValueTables.h"
 #include "../../CloudSeed/AudioLib/MathDefs.h"
 
+#define PERFORMANCE_MONITOR 1
+
 using namespace daisy;
 using namespace daisysp;
 
 DaisySeed hw;
-enum Inputs {
+enum class Inputs: int {
     dryOut = 0,     // Dry / Wet
     earlyOut,       // Early reverb lvl
     mainOut,        // Late reverb lvl
     feedback,       // Late reverb feedback
     tapDecay,       // Early reverb dampening
     time,           // Late reverb decay
-    INPUTS_COUNT,
+    Count,
 };
-float currentValues[INPUTS_COUNT],
-      previousValues[INPUTS_COUNT];
+float currentValues[(int)Inputs::Count],
+      previousValues[(int)Inputs::Count];
 
 struct GPIOPin {
     daisy::Pin pin;
@@ -38,6 +40,10 @@ int currentPresetIndex;
 const size_t blockSize = 48;
 float ins[blockSize];
 float outs[blockSize];
+
+#ifdef PERFORMANCE_MONITOR
+CpuLoadMeter loadMeter;
+#endif
 
 CloudSeed::ReverbController *reverb = 0;
 
@@ -105,12 +111,12 @@ void cyclePreset()
 void readAdcValues()
 {
     // TODO use a one pole filter to smooth those out (calling freq is sampleRate / blockSize)
-    currentValues[dryOut] = hw.adc.GetFloat(dryOut);
-    currentValues[earlyOut] = hw.adc.GetFloat(earlyOut);
-    currentValues[mainOut] = hw.adc.GetFloat(mainOut);
-    currentValues[feedback] = hw.adc.GetFloat(feedback);
-    currentValues[tapDecay] = hw.adc.GetFloat(tapDecay);
-    currentValues[time] = hw.adc.GetFloat(time);
+    currentValues[(int)Inputs::dryOut] = hw.adc.GetFloat((int)Inputs::dryOut);
+    currentValues[(int)Inputs::earlyOut] = hw.adc.GetFloat((int)Inputs::earlyOut);
+    currentValues[(int)Inputs::mainOut] = hw.adc.GetFloat((int)Inputs::mainOut);
+    currentValues[(int)Inputs::feedback] = hw.adc.GetFloat((int)Inputs::feedback);
+    currentValues[(int)Inputs::tapDecay] = hw.adc.GetFloat((int)Inputs::tapDecay);
+    currentValues[(int)Inputs::time] = hw.adc.GetFloat((int)Inputs::time);
 }
 
 void updateReverbParameters()
@@ -119,12 +125,12 @@ void updateReverbParameters()
 
     // TODO Code used to check if parameter changed from previous value before updating
     // reverb params, check if needed and if so use a threshold value to check if changed
-    reverb->SetParameter(::Parameter::DryOut, currentValues[dryOut]);
-    reverb->SetParameter(::Parameter::EarlyOut, currentValues[earlyOut]);
-    reverb->SetParameter(::Parameter::MainOut, currentValues[mainOut]);
-    reverb->SetParameter(::Parameter::LateDiffusionFeedback, currentValues[feedback]);
-    reverb->SetParameter(::Parameter::TapDecay, currentValues[tapDecay]);
-    reverb->SetParameter(::Parameter::LineDecay, currentValues[time]);
+    reverb->SetParameter(::Parameter::DryOut, currentValues[(int)Inputs::dryOut]);
+    reverb->SetParameter(::Parameter::EarlyOut, currentValues[(int)Inputs::earlyOut]);
+    reverb->SetParameter(::Parameter::MainOut, currentValues[(int)Inputs::mainOut]);
+    reverb->SetParameter(::Parameter::LateDiffusionFeedback, currentValues[(int)Inputs::feedback]);
+    reverb->SetParameter(::Parameter::TapDecay, currentValues[(int)Inputs::tapDecay]);
+    reverb->SetParameter(::Parameter::LineDecay, currentValues[(int)Inputs::time]);
 }
 
 void updateSwitches()
@@ -141,6 +147,10 @@ static void AudioCallback(AudioHandle::InputBuffer  in,
                           AudioHandle::OutputBuffer out,
                           size_t                    size)
 {
+    #ifdef PERFORMANCE_MONITOR
+    loadMeter.OnBlockStart();
+    #endif
+
     updateReverbParameters();
     updateSwitches();
 
@@ -167,18 +177,22 @@ static void AudioCallback(AudioHandle::InputBuffer  in,
             out[0][i] = in[0][i];
         }
     }
+
+#ifdef PERFORMANCE_MONITOR
+    loadMeter.OnBlockEnd();
+#endif
 }
 
 void AdcInit()
 {
-    AdcChannelConfig adcChannelConfig[INPUTS_COUNT];
-    adcChannelConfig[dryOut].InitSingle(daisy::seed::A0);
-    adcChannelConfig[earlyOut].InitSingle(daisy::seed::A1);
-    adcChannelConfig[mainOut].InitSingle(daisy::seed::A2);
-    adcChannelConfig[feedback].InitSingle(daisy::seed::A3);
-    adcChannelConfig[tapDecay].InitSingle(daisy::seed::A4);
-    adcChannelConfig[time].InitSingle(daisy::seed::A5);
-    hw.adc.Init(adcChannelConfig, INPUTS_COUNT);
+    AdcChannelConfig adcChannelConfig[(int)Inputs::Count];
+    adcChannelConfig[(int)Inputs::dryOut].InitSingle(daisy::seed::A0);
+    adcChannelConfig[(int)Inputs::earlyOut].InitSingle(daisy::seed::A1);
+    adcChannelConfig[(int)Inputs::mainOut].InitSingle(daisy::seed::A2);
+    adcChannelConfig[(int)Inputs::feedback].InitSingle(daisy::seed::A3);
+    adcChannelConfig[(int)Inputs::tapDecay].InitSingle(daisy::seed::A4);
+    adcChannelConfig[(int)Inputs::time].InitSingle(daisy::seed::A5);
+    hw.adc.Init(adcChannelConfig, (int)Inputs::Count);
 }
 
 void GPIOInit()
@@ -198,6 +212,11 @@ int main(void)
     hw.SetAudioBlockSize(blockSize);
     float sampleRate = hw.AudioSampleRate();
 
+    #ifdef PERFORMANCE_MONITOR
+    hw.StartLog();
+    loadMeter.Init(sampleRate, blockSize);
+    #endif
+
     AudioLib::ValueTables::Init();
     CloudSeed::FastSin::Init();
 
@@ -211,5 +230,19 @@ int main(void)
     hw.adc.Start();
     hw.StartAudio(AudioCallback);
 
-    for (;;) {}
+    for (;;) {
+        #ifdef PERFORMANCE_MONITOR
+        // get the current load (smoothed value and peak values)
+        const float avgLoad = loadMeter.GetAvgCpuLoad();
+        const float maxLoad = loadMeter.GetMaxCpuLoad();
+        const float minLoad = loadMeter.GetMinCpuLoad();
+        // print it to the serial connection (as percentages)
+        hw.PrintLine("Processing Load %:");
+        hw.PrintLine("Max: " FLT_FMT3, FLT_VAR3(maxLoad * 100.0f));
+        hw.PrintLine("Avg: " FLT_FMT3, FLT_VAR3(avgLoad * 100.0f));
+        hw.PrintLine("Min: " FLT_FMT3, FLT_VAR3(minLoad * 100.0f));
+        // don't spam the serial connection too much
+        System::Delay(500);
+        #endif
+    }
 }
